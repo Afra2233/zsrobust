fcf76789a45665a8e4c84e163985c9549af0ea49
afra818 <60651664+afra818@users.noreply.github.com>
2026-03-29 20:46:54 +0100

    f

--- PMG_AFT.py
+++ PMG_AFT.py
@@ -320,16 +320,16 @@ def main():
 
     val_dataset_list = []
     if args.evaluate:
-        val_dataset_name = ['cifar10','cifar100','STL10','PCAM','fgvc_aircraft','Food101','flowers102','dtd','oxfordpet']
+        val_dataset_name = ['cifar10','cifar100','STL10','fgvc_aircraft','Food101']
         # 'fgvc_aircraft','oxfordpet','Food101','StanfordCars'
     # 'SUN397','ImageNet','Caltech101'，'Caltech256','EuroSAT', 'cifar100', 'STL10',  'Food101',
                             # 'oxfordpet', 'flowers102', 'dtd', 'fgvc_aircraft',
-                            # 'tinyImageNet','Caltech101','Caltech256', 'StanfordCars', 'PCAM'] 
+                            # 'tinyImageNet','Caltech101','Caltech256', 'StanfordCars', 'PCAM''flowers102','dtd','oxfordpet'] 
     else:
-        val_dataset_name = ['cifar10','cifar100','STL10','PCAM','fgvc_aircraft','Food101','flowers102','dtd','oxfordpet']
+        val_dataset_name = ['cifar10','cifar100','STL10','fgvc_aircraft','Food101']
         # ['cifar10', 'cifar100', 'STL10', 'Food101',
                             # 'oxfordpet', 'flowers102', 'dtd', 'fgvc_aircraft',
-                            # 'tinyImageNet', 'Caltech101','Caltech256','StanfordCars', 'PCAM']
+                            # 'tinyImageNet', 'Caltech101','Caltech256','StanfordCars', 'PCAM','flowers102','dtd','oxfordpet']
     # 'SUN397','ImageNet', 'Caltech101'，'Caltech256','EuroSAT',
     for each in val_dataset_name:
         if each == 'cifar10':
